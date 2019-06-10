4768481be29c1779879b615dd9880e87107a414e
Maria de Lourdes Atilano Hernandez <maria.atilano@globant.com>
2019-06-10 15:53:02 -0500

    created setupTest file, added jest-cli, modified configuration of jest.config.js and fixed indentation on html

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzyemAdapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import App from './App';
 
-Enzyme.configure({ adapter: new EnzyemAdapter() });
 
 test('render without error', () => {
     const wrapper = shallow(<App />);
