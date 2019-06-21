fccdde3d5ab24d183fca75fa58e1f09f4776a7e5
Maria de Lourdes Atilano Hernandez <maria.atilano@globant.com>
2019-06-21 17:05:50 -0500

    add code and tests to clear input box

--- src/Input.test.js
+++ src/Input.test.js
@@ -122,9 +122,13 @@ describe('`guessWord` action creator call' ,() => {
   });
 
   test('call `guessWord` with input value as argument', () => {
-    console.log(guessWordMock.mock.calls);
     const guessedWordArg = guessWordMock.mock.calls[0][0];
 
     expect(guessedWordArg).toBe(guessedWord);
+  });
+
+  test('input box clears on submit', () => {
+    
+    expect(wrapper.instance().inputBox.current.value).toBe('');
   });
 });
