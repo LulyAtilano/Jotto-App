b9c8c97304cb962070971664e717f627778816fc
Maria de Lourdes Atilano Hernandez <maria.atilano@globant.com>
2019-06-21 15:09:22 -0500

    connect App component (and test)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React, { Component} from "react";
+import { connect } from 'react-redux';
 import {hot} from "react-hot-loader";
 import "./App.css";
 
 import GuessedWords from './GuessedWords';
 import Congrats from './Congrats';
-
+import { getSecretWord } from './actions';
 
 class App extends Component{
   render(){
     return(
       <div className="container">
         <h1> Jotto </h1>
-        <Congrats success={ true } />
-        <GuessedWords guessedWords={[
-          { guessedWord: 'train', letterMatchCount: 3 }
-        ]}/>
+        <Congrats success={this.props.success} />
+        <GuessedWords guessedWords={this.props.guessedWords} />
       </div>
     );
   }
 }
 
-export default hot(module)(App);
+const mapStateToProps = (state) => {
+  const { success, guessedWords, secretWord } = state;
+  return { success, guessedWords, secretWord };
+}
+
+export default connect(mapStateToProps, { getSecretWord })(App);
